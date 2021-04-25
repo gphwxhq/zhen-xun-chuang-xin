80bddb6ceee73be7cec420c16c489234eb0dd476
unknown <unknown@example.com>
2021-04-25 17:16:12 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// app.js
+App({
+  onLaunch(){
+    wx.cloud.init({
+      env:"main-2gjpci0p59828101"
+    })
+  },
+})
---
+++ cloudfile/all/functions/search_in_teacher/index.js
@@ -0,0 +1,28 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env:"main-2gjpci0p59828101"
+})
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  try{
+    res=await cloud.database().collection("teacher").field({
+    "学者名单":true,
+    "工作单位":true,
+    "其他":true
+  }).where({
+       "学者名单":event.name
+  }).limit(1).get()
+  console.log(res.data)
+  return{
+    name:res.data[0].学者名单,
+    dep:res.data[0].工作单位,
+    oth:res.data[0].其他
+  }
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
