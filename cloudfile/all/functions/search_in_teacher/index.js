2b3631d16235c1e7f12cd82bab013f36de7a2e5c
gphwxhq <yangboyuanyang@126.com>
2021-04-29 22:35:30 +0800

    new

--- cloudfile/all/functions/search_in_teacher/index.js
+++ cloudfile/all/functions/search_in_teacher/index.js
@@ -8,14 +8,13 @@ cloud.init({
 // 云函数入口函数
 exports.main = async (event, context) => {
   try{
-    res=await cloud.database().collection("teacher").field({
+    res=await cloud.database().collection("teachers").field({
     "学者名单":true,
     "工作单位":true,
     "其他":true
   }).where({
        "学者名单":event.name
   }).limit(1).get()
-  console.log(res.data)
   return{
     name:res.data[0].学者名单,
     dep:res.data[0].工作单位,
